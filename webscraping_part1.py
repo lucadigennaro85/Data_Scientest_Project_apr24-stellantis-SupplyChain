17cf7d8329d3b924078af6410b11aefad42d7500
Luca Di Gennaro <luca.digennaro@stellantis.com>
2024-10-14 11:33:14 +0200

    Bug fixing

--- webscraping_part1.py
+++ webscraping_part1.py
@@ -7,48 +7,47 @@ url = 'https://www.trustpilot.com/categories/atm'
 
 # Send a request to the webpage
 response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, "lxml")
 
 # Find all company listings
-companies = soup.find_all('div', class_='styles_main__XgQiuO')
+companies = soup.find_all('div', class_='styles_businessUnitMain__PuwB7')
 
 for company in companies:
     name = company.find('p', class_='typography_heading-xs__jSwUz typography_appearance-default__AAY17').text
     domain = company.find('p', class_='typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l styles_websiteUrlDisplayed__QqkCT').text
-    reviews = company.find('span', class_='styles_separator__TG_uV')
-    trustscore = company.find('div', class_='styles_rating__1x9gC').text
+    reviews = company.find('p', class_='typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l styles_ratingText__yQ5S7').text.split("|")[1].split(" ")[0]
+    trustscore = company.find('span', class_='typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l styles_trustScore__8emxJ').text.split(" ")[1]
 
-    print(f"Company: {name}")
-    print(f"Domain: {domain}")
-    print(f"Number of Reviews: {reviews}")
-    print(f"Trustscore: {trustscore}")
+    print("Company: ", name)
+    print("Domain: ", domain)
+    print("Number of Reviews: ", reviews)
+    print("Trustscore: ", trustscore)
 
-    url = os.path.join('https://www.trustpilot.com/review/', domain)
+    url = 'https://www.trustpilot.com/review/' + domain
 
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, "lxml")
 
-    reviews_classes = soup.find_all('div', class_='styles_row__wvn4i')
+    reviews_classes = soup.find_all('label', class_='styles_row__wvn4i')
 
     for reviews_class in reviews_classes:
         
         if reviews_class['data-star-rating'] == 'five':
             class5_reviews_perc = reviews_class.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_cell__qnPHy styles_percentageCell__cHAnb').text
-        
+            print("5 stars reviews percentage: ", class5_reviews_perc)
+            
         if reviews_class['data-star-rating'] == 'four':
             class4_reviews_perc = reviews_class.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_cell__qnPHy styles_percentageCell__cHAnb').text
-
+            print("4 stars reviews percentage: ", class4_reviews_perc)
+            
         if reviews_class['data-star-rating'] == 'three':
             class3_reviews_perc = reviews_class.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_cell__qnPHy styles_percentageCell__cHAnb').text
-
+            print("3 stars reviews percentage: ", class3_reviews_perc)
+            
         if reviews_class['data-star-rating'] == 'two':
             class2_reviews_perc = reviews_class.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_cell__qnPHy styles_percentageCell__cHAnb').text
-
+            print("2 stars reviews percentage: ", class2_reviews_perc)
+            
         if reviews_class['data-star-rating'] == 'one':
             class1_reviews_perc = reviews_class.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_cell__qnPHy styles_percentageCell__cHAnb').text
-
-    print(f"5 stars reviews percentage: {class5_reviews_perc}")
-    print(f"4 stars reviews percentage: {class4_reviews_perc}")
-    print(f"3 stars reviews percentage: {class3_reviews_perc}")
-    print(f"2 stars reviews percentage: {class2_reviews_perc}")
-    print(f"1 stars reviews percentage: {class1_reviews_perc}")
+            print("1 stars reviews percentage: ", class1_reviews_perc)
--- webscraping_part2.py
+++ webscraping_part2.py
@@ -1,27 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL of the company page
-url = 'https://www.trustpilot.com/review/www.showroomprive.com'
-
-# Send a request to the webpage
+pageNumber = 0
+url = 'https://www.trustpilot.com/review/gohenry.com'
 response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
 
-# Find all reviews
-reviews = soup.find_all('div', class_='styles_cardWrapper__LcCPA styles_show__HUXRb styles_reviewCard__9HxJJ')
+# URL of the company page
+while response.status_code == 200:
+    pageNumber += 1
+    url = url + '?page=' + str(pageNumber)
 
-for review in reviews:
-    stars = review.find('img', class_='star-rating_starRating__4rrcf star-rating_medium__iN6Ty')['alt'][7]
-    comment = review.find('p', class_='typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn').text
-    response = review.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_message__shHhX')
+    # Send a request to the webpage
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "lxml")
 
-    print(f"Stars: {stars}")
-    print(f"Comment: {comment}")
+    # Find all reviews
+    reviews = soup.find_all('div', class_='styles_reviewCardInner__EwDq2')
 
-    if int(stars) <= 2:
-        if response:
-            print("Company has responded to this review.")
+    for review in reviews:
+        stars = review.find('div', class_='star-rating_starRating__4rrcf star-rating_medium__iN6Ty').find('img')['alt'][6]
+        if (review.find('p', class_='typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn')):
+            comment = review.find('p', class_='typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn').text
         else:
-            print("Company has NOT responded to this review.")
-        print("\n")
+            comment = ""
+        response = review.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_message__shHhX')
+
+        print("Stars: ", stars)
+        print("Comment: ", comment)
+
+        if int(stars) <= 2:
+            if response:
+                print("Company has responded to this review.")
+            else:
+                print("Company has NOT responded to this review.")
