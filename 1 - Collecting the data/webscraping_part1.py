321e9cf310fb65b4f38a3b01516234abe2079059
Luca Di Gennaro <luca.digennaro@stellantis.com>
2025-01-31 18:10:07 +0100

    Script updated with new class names. Class names on trustpilot.com seems to have a randomly generated part

--- 1 - Collecting the data/webscraping_part1.py
+++ 1 - Collecting the data/webscraping_part1.py
@@ -7,16 +7,16 @@ url = 'https://www.trustpilot.com/categories/atm'
 
 # Send a request to the webpage
 response = requests.get(url)
-soup = BeautifulSoup(response.text, "lxml")
+soup = BeautifulSoup(response.text, "html.parser")
 
 # Find all company listings
-companies = soup.find_all('div', class_='styles_businessUnitMain__PuwB7')
+companies = soup.find_all('div', class_='styles_businessUnitMain__e_tIa')
 
 for company in companies:
-    name = company.find('p', class_='typography_heading-xs__jSwUz typography_appearance-default__AAY17').text
-    domain = company.find('p', class_='typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l styles_websiteUrlDisplayed__QqkCT').text
-    reviews = company.find('p', class_='typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l styles_ratingText__yQ5S7').text.split("|")[1].split(" ")[0]
-    trustscore = company.find('span', class_='typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l styles_trustScore__8emxJ').text.split(" ")[1]
+    name = company.find('p', class_='typography_heading-xs__osRhC typography_appearance-default__t8iAq').text
+    domain = company.find('p', class_='typography_body-m__k2UI7 typography_appearance-subtle__PYOVM styles_websiteUrlDisplayed__lSw1A').text
+    reviews = company.find('p', class_='typography_body-m__k2UI7 typography_appearance-subtle__PYOVM styles_ratingText__A2dmB').text.split("|")[1].split(" ")[0]
+    trustscore = company.find('span', class_='typography_body-m__k2UI7 typography_appearance-subtle__PYOVM styles_trustScore__iURkS').text.split(" ")[1]
 
     print("Company: ", name)
     print("Domain: ", domain)
@@ -26,28 +26,28 @@ for company in companies:
     url = 'https://www.trustpilot.com/review/' + domain
 
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, "lxml")
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    reviews_classes = soup.find_all('label', class_='styles_row__wvn4i')
+    reviews_classes = soup.find_all('label', class_='styles_row__4BwV6')
 
     for reviews_class in reviews_classes:
         
         if reviews_class['data-star-rating'] == 'five':
-            class5_reviews_perc = reviews_class.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_cell__qnPHy styles_percentageCell__cHAnb').text
+            class5_reviews_perc = reviews_class.find('p', class_='typography_body-m__k2UI7 typography_appearance-default__t8iAq styles_cell__2f_al styles_percentageCell__AKkqm').text
             print("5 stars reviews percentage: ", class5_reviews_perc)
             
         if reviews_class['data-star-rating'] == 'four':
-            class4_reviews_perc = reviews_class.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_cell__qnPHy styles_percentageCell__cHAnb').text
+            class4_reviews_perc = reviews_class.find('p', class_='typography_body-m__k2UI7 typography_appearance-default__t8iAq styles_cell__2f_al styles_percentageCell__AKkqm').text
             print("4 stars reviews percentage: ", class4_reviews_perc)
             
         if reviews_class['data-star-rating'] == 'three':
-            class3_reviews_perc = reviews_class.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_cell__qnPHy styles_percentageCell__cHAnb').text
+            class3_reviews_perc = reviews_class.find('p', class_='typography_body-m__k2UI7 typography_appearance-default__t8iAq styles_cell__2f_al styles_percentageCell__AKkqm').text
             print("3 stars reviews percentage: ", class3_reviews_perc)
             
         if reviews_class['data-star-rating'] == 'two':
-            class2_reviews_perc = reviews_class.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_cell__qnPHy styles_percentageCell__cHAnb').text
+            class2_reviews_perc = reviews_class.find('p', class_='typography_body-m__k2UI7 typography_appearance-default__t8iAq styles_cell__2f_al styles_percentageCell__AKkqm').text
             print("2 stars reviews percentage: ", class2_reviews_perc)
             
         if reviews_class['data-star-rating'] == 'one':
-            class1_reviews_perc = reviews_class.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_cell__qnPHy styles_percentageCell__cHAnb').text
+            class1_reviews_perc = reviews_class.find('p', class_='typography_body-m__k2UI7 typography_appearance-default__t8iAq styles_cell__2f_al styles_percentageCell__AKkqm').text
             print("1 star reviews percentage: ", class1_reviews_perc)
--- 1 - Collecting the data/webscraping_part2.py
+++ 1 - Collecting the data/webscraping_part2.py
@@ -12,18 +12,18 @@ while response.status_code == 200:
 
     # Send a request to the webpage
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, "lxml")
+    soup = BeautifulSoup(response.text, "html.parser")
 
     # Find all reviews
-    reviews = soup.find_all('div', class_='styles_reviewCardInner__EwDq2')
+    reviews = soup.find_all('div', class_='styles_reviewCardInner__UZk1x')
 
     for review in reviews:
-        stars = review.find('div', class_='star-rating_starRating__4rrcf star-rating_medium__iN6Ty').find('img')['alt'][6]
-        if (review.find('p', class_='typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn')):
-            comment = review.find('p', class_='typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn').text
+        stars = review.find('div', class_='star-rating_starRating__sdbkn star-rating_medium__Oj7C9').find('img')['alt'][6]
+        if (review.find('p', class_='typography_body-l__v5JLj typography_appearance-default__t8iAq typography_color-black__wpn7m')):
+            comment = review.find('p', class_='typography_body-l__v5JLj typography_appearance-default__t8iAq typography_color-black__wpn7m').text
         else:
             comment = ""
-        response = review.find('p', class_='typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_message__shHhX')
+        response = review.find('p', class_='typography_body-m__k2UI7 typography_appearance-default__t8iAq styles_message____SVk')
 
         print("Stars: ", stars)
         print("Comment: ", comment)
