b8a0109964a11b63d5af9e5d0f32d66030d549ce
Luca Di Gennaro <luca.digennaro@stellantis.com>
2025-04-10 18:26:35 +0200

    Combined_score included in the graphs, new "combined" csv used for all graphs

--- graph.py
+++ graph.py
@@ -10,6 +10,7 @@ df_unique = df.drop_duplicates(subset='Company')
 
 max_reviews = df_unique['Number of Reviews'].max()
 max_trustscore = df_unique['Trustscore'].max()
+max_combinedscore = df_unique['Combined_Score'].max()
 max_5stars = df_unique['5 stars reviews percentage'].max()
 max_4stars = df_unique['4 stars reviews percentage'].max()
 max_3stars = df_unique['3 stars reviews percentage'].max()
@@ -27,6 +28,7 @@ app.layout = html.Div([
     
     dcc.Graph(id='bar-chart-reviews'),
     dcc.Graph(id='bar-chart-trustscore'),
+    dcc.Graph(id='bar-chart-combinedscore'),
     dcc.Graph(id='scatter-plot-trustscore-reviews'),
     dcc.Graph(id='bar-chart-stars'),
     dcc.Graph(id='bar-chart-sentiment')
@@ -35,6 +37,7 @@ app.layout = html.Div([
 @app.callback(
     Output('bar-chart-reviews', 'figure'),
     Output('bar-chart-trustscore', 'figure'),
+    Output('bar-chart-combinedscore', 'figure'),
     Output('scatter-plot-trustscore-reviews', 'figure'),
     Output('bar-chart-stars', 'figure'),
     Output('bar-chart-sentiment', 'figure'),
@@ -47,21 +50,24 @@ def update_graphs(_):
     fig2 = px.bar(df_unique.sort_values(by='Trustscore', ascending=False), y='Trustscore', x='Company', title='Trustscore')
     fig2.update_layout(yaxis=dict(range=[0, max_trustscore]))
 
-    fig3 = px.scatter(df_unique, x='Number of Reviews', y='Trustscore', color='Company', title='Trustscore and Number of reviews')
+    fig3 = px.bar(df_unique.sort_values(by='Combined_Score', ascending=False), y='Combined_Score', x='Company', title='Combined Score')
+    fig3.update_layout(yaxis=dict(range=[0, max_combinedscore]))
 
-    fig4 = go.Figure(data=[
+    fig4 = px.scatter(df_unique, x='Number of Reviews', y='Trustscore', color='Company', title='Trustscore and Number of reviews')
+
+    fig5 = go.Figure(data=[
         go.Bar(name='5 Stars', x=df_unique['Company'], y=df_unique['5 stars reviews percentage']),
         go.Bar(name='4 Stars', x=df_unique['Company'], y=df_unique['4 stars reviews percentage']),
         go.Bar(name='3 Stars', x=df_unique['Company'], y=df_unique['3 stars reviews percentage']),
         go.Bar(name='2 Stars', x=df_unique['Company'], y=df_unique['2 stars reviews percentage']),
         go.Bar(name='1 Star', x=df_unique['Company'], y=df_unique['1 star reviews percentage'])
     ])
-    fig4.update_layout(barmode='stack', title='Stars distribution per Company', yaxis=dict(range=[0, max_stars]))
+    fig5.update_layout(barmode='stack', title='Stars distribution per Company', yaxis=dict(range=[0, max_stars]))
 
-    fig5 = px.bar(df_unique, x='Company', y=['VADER Sentiment Score', 'TextBlob Sentiment Score', 'BERT Sentiment Score'], title='Sentiment analysis scores per Company')    
-    fig5.update_layout(yaxis=dict(range=[-1, max(max_vader, max_textblob, max_bert)]))
+    fig6 = px.bar(df_unique, x='Company', y=['VADER Sentiment Score', 'TextBlob Sentiment Score', 'BERT Sentiment Score'], title='Sentiment analysis scores per Company')    
+    fig6.update_layout(yaxis=dict(range=[-1, max(max_vader, max_textblob, max_bert)]))
 
-    return fig1, fig2, fig3, fig4, fig5
+    return fig1, fig2, fig3, fig4, fig5, fig6
 
 if __name__ == '__main__':
     app.run(debug=False)
